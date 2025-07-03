f5b80fdadc75765aba5778977c2ab2e9f1518146
IvanGabriel27 <128513972+IvanGabriel27@users.noreply.github.com>
2025-07-03 11:16:05 -0300

    inicio

--- script.js
+++ script.js
@@ -4,4 +4,35 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
+const perguntas = [
+    {
+        enunciado: "Quem foi o campeão olímpico em Tóquio no ano de 2021?",
+        alternativas: [
+            {
+                texto:"Italo Ferreira",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Gabriel Medina",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+     {
+        enunciado: "Quem foi o campeão olímpico em Tóquio no ano de 2021?",
+        alternativas: [
+            {
+                texto:"Italo Ferreira",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Gabriel Medina",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+]
