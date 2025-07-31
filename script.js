eb7d9e0955ec15f0243f6b8c8ab6ab30de2a809b
IvanGabriel27 <128513972+IvanGabriel27@users.noreply.github.com>
2025-07-31 08:35:29 -0300

    .

--- script.js
+++ script.js
@@ -35,4 +35,49 @@ const perguntas = [
             }
         ]
     },
+    {
+        enunciado: "Quem foi o campeão olímpico em Tóquio no ano de 2021?",
+        alternativas: [
+            {
+                texto:"Italo Ferreira",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Gabriel Medina",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+    {
+        enunciado: "Quem foi o campeão olímpico em Tóquio no ano de 2021?",
+        alternativas: [
+            {
+                texto:"Italo Ferreira",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Gabriel Medina",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+    {
+        enunciado: "Quem foi o campeão olímpico em Tóquio no ano de 2021?",
+        alternativas: [
+            {
+                texto:"Italo Ferreira",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"Gabriel Medina",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
 ]
